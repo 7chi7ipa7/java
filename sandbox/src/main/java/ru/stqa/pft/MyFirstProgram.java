d823f00150928075851bde9126c353e8d33dcca4
7chi7ipa7 <62651528+7chi7ipa7@users.noreply.github.com>
2020-08-03 18:18:13 +0300

    конец первого занятия

--- sandbox/src/main/java/ru/stqa/pft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/MyFirstProgram.java
@@ -1,7 +1,17 @@
 package ru.stqa.pft;
 
 public class MyFirstProgram {
+
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        hello("world");
+        hello("user");
+        Square s = new Square(5);
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь квадрата " + s.l + " = " + s.area());
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + ", " + r.b + " = " + r.area());
     }
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
 }
